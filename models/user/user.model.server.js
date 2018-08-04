ddd2ea7349205ad99428b7e82064feac39c1d4d0
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-04 01:52:15 -0400

    Created user model and schema

---
+++ models/user/user.model.server.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const userSchema = require('./user.schema.server');
+
+const userModel = mongoose.model('UserModel', userSchema);
+
+findAllUsers = () =>
+userModel.find();
+
+findUserByCredentials = (username, password) =>
+userModel.findOne({username: username, password: password});
+
+findUserById = userId =>
+userModel.findById(userId)
+
+findUserByIdExpanded = userId =>
+userModel
+    .findById(userId)
+    .populate('sections')
+    .exec()
+
+createUser = (user) => {
+    return userModel.create(user);
+}
+
+findUserByUsername = (username) => {
+    userModel.findOne({username: username});
+}
+
+module.exports = {
+    findUserByIdExpanded,
+    findUserById,
+    findAllUsers,
+    findUserByCredentials,
+    createUser,
+    findUserByUsername
+};
