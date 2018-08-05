e294d9f02ab76a31e5b09d993c6a9a326952df70
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-05 16:51:32 -0400

    Added enrollment methods

--- models/section/section.model.server.js
+++ models/section/section.model.server.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const sectionSchema = require('./section.schema.server');
 const sectionModel = mongoose.model('SectionModel', sectionSchema);
 
-
 createSection = (section) =>
 sectionModel.create(section);
 
@@ -13,16 +12,37 @@ deleteSection = (sectionId) =>
 sectionModel.findByIdAndRemove(sectionId)
 
 updateSection = (section) =>
-sectionModel.update({
-        _id: section.id
-    },
-    {
-        $set: {title: section.title, maxCap: section.maxCap, rem: section.rem}
+{
+    sectionModel.update({
+            _id: section.id
+        },
+        {
+            $set: {title: section.title, maxCap: section.maxCap, rem: section.rem}
+        });
+}
+
+decrementSectionCapacity = (sectionId) => {
+    return sectionModel.update({
+        _id: sectionId
+    }, {
+        $inc: {rem: -1}
     });
+}
+
+incrementSectionCapacity = (sectionId) => {
+    return sectionModel.update({
+        _id: sectionId
+    }, {
+        $inc: {rem: +1}
+    });
+}
+
 
 module.exports = {
     createSection,
     findSectionsForCourse,
     deleteSection,
-    updateSection
+    updateSection,
+    decrementSectionCapacity,
+    incrementSectionCapacity
 };
