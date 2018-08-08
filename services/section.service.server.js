b00abf6bd22844f98ec531a0a20b5d3043aa1073
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-07 21:03:43 -0400

    Updated endpoint API for section enrollment

--- services/section.service.server.js
+++ services/section.service.server.js
@@ -5,9 +5,9 @@ module.exports = function (app) {
     app.get('/api/section/:sectionId', findSectionById);
     app.delete('/api/section/:sectionId', deleteSection);
     app.put('/api/section/:sectionId', updateSection);
-    app.put('/api/section/:sectionId/enroll', enrollStudentInSection);
-    app.delete('/api/section/:sectionId/unenroll', removeStudentFromSection);
-    app.get('/api/student/section', findAllSectionsForStudent);
+    app.post('/api/student/:sid/section/:kid', enrollStudentInSection);
+    app.delete('/api/student/:sid/section/:kid', removeStudentFromSection);
+    app.get('/api/student/:sid/section', findAllSectionsForStudent);
 
     var sectionModel = require('../models/section/section.model.server');
     var enrollmentModel = require('../models/enrollment/enrollment.model.server');
@@ -59,9 +59,8 @@ module.exports = function (app) {
     }
 
     function removeStudentFromSection(req, res) {
-        var sectionId = req.params['sectionId'];
-        var currentUser = req.session['currentUser'];
-        var studentId = currentUser._id;
+        var sectionId = req.params['kid'];
+        var studentId = req.session['sid'];
         var enrollment = {
             student: studentId,
             section: sectionId
@@ -79,9 +78,8 @@ module.exports = function (app) {
     }
 
     function enrollStudentInSection(req, res) {
-        var sectionId = req.params['sectionId'];
-        var currentUser = req.session['currentUser'];
-        var studentId = currentUser._id;
+        var sectionId = req.params['kid'];
+        var studentId = req.session['sid'];
         var enrollment = {
             student: studentId,
             section: sectionId
@@ -99,8 +97,7 @@ module.exports = function (app) {
     }
 
     function findAllSectionsForStudent(req, res) {
-        var currentUser = req.session['currentUser'];
-        var studentId = currentUser._id;
+        var studentId = req.session['sid'];
         enrollmentModel
             .findSectionsForStudent(studentId)
             .then(function (enrollments) {
