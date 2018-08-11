ba6ec28c63fa853a02395d9bb927b68647fb1bea
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-11 16:04:11 -0400

    Added findquizbyid and submit methods

--- services/quiz.service.server.js
+++ services/quiz.service.server.js
@@ -1,8 +1,28 @@
 module.exports = app => {
     var quizzes = require('./quizzes.json');
+    var submissionModel = require('../models/quiz/submission.model.server');
+
     app.get('/api/quiz', findAllQuizzes);
+    app.get('/api/quiz/:quizId', findQuizById);
+    app.post('/api/quiz/:quizId', submitQuiz);
 
     function findAllQuizzes(req, res) {
         res.json(quizzes);
+    }
+
+    function findQuizById(req, res) {
+        var quiz = quizzes.filter(function (q) {
+            return q._id == req.params.quizId });
+        res.json(quiz[0]);
+    }
+
+    function submitQuiz(req, res) {
+        var submission = req.body;
+        var quizId = req.params.quizId;
+        submissionModel
+            .submitQuiz(submission, quizId, 'alice')
+            .then(function (submission) {
+                res.json(submission);
+            })
     }
 };
