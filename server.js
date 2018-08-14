1b796722ecd7083cc65388908b4a99231f55c6ba
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-08-14 15:00:51 -0400

    Added new services

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://wbdv-angular-client-mkaur.herokuapp.com");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -31,5 +32,6 @@ userService(app);
 require('./services/section.service.server')(app);
 require('./services/quiz.service.server')(app);
 require('./services/submission.service.server')(app);
+require('./services/question.service.server')(app);
 
 app.listen(process.env.PORT || 3000)
